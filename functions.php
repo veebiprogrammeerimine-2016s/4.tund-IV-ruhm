8c7710dedbd37b36982db0686bf4151333b58f3a
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2016-09-28 15:19:11 +0300

    added login fn

--- functions.php
+++ functions.php
@@ -20,6 +20,48 @@
 		
 	}
 	
+	function login($email, $password) {
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("
+			SELECT id, email, password, created
+			FROM user_sample
+			WHERE email = ?
+		");
+		
+		echo $mysqli->error;
+		
+		//asendan küsimärgi
+		$stmt->bind_param("s", $email);
+		
+		//rea kohta tulba väärtus
+		$stmt->bind_result($id, $emailFromDb, $passwordFromDb, $created);
+		
+		$stmt->execute();
+		
+		//ainult SELECT'i puhul
+		if($stmt->fetch()) {
+			// oli olemas, rida käes
+			//kasutaja sisestas sisselogimiseks
+			$hash = hash("sha512", $password);
+			
+			if ($hash == $passwordFromDb) {
+				echo "Kasutaja $id logis sisse";
+			} else {
+				echo "parool vale";
+			}
+			
+			
+		} else {
+			
+			//ei olnud ühtegi rida
+			echo "Sellise emailiga ".$email." kasutajat ei ole olemas";
+		}
+		
+		
+	}
+	
 	
 	
 	
